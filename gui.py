7ab6ff98794c54b6de1fb6cdc2adc34c7d2d633c
James <122633834+jmst@users.noreply.github.com>
2025-02-12 21:47:51 +0000

    Combined line, spinbox and associated variables into dictionary
    Better convert this into a class.

--- gui.py
+++ gui.py
@@ -116,7 +116,7 @@ class TkGui:
         ### image canvas
         # make hc mode opt to use bitmap (like pdf.js)
         ogpdf = PhotoImage(master=root, file='t-0.png') # master=self.im_canvas
-        im_scrollbut['command']=lambda: self.image_scale(root, ogpdf, 0)
+        im_scrollbut['command'] = lambda: self.image_scale(root, ogpdf, 0)
         self.lt = 5 # line thickness
         # anchor nw for guillotine metaphor (opt)
         self.pdfview = self.canvas.create_image(self.lt, self.lt, anchor='nw')
@@ -138,155 +138,137 @@ class TkGui:
         # line coords drawn from center of line width, (0,0) cuts off left edge
         debug_lt = self.lt
         lineo = self.lt//2 #-(self.lt%2)
-        # print(lineo)
-        lineoffset = self.lt*4 # don't know why lt*4 here
+        lineoffset = self.bd * 2 # self.lt*4 # don't know why lt*4 here
         x0 = lineo
         y0 = lineo
-        x1 = int(self.canvas['width']) - lineo - 1 + lineoffset # why the -1?
-        y1 = int(self.canvas['height']) + self.lt * 3 + lineo
+        x1 = int(self.canvas['width']) + lineoffset - lineo - 1 # 1 for 0-index?
+        y1 = int(self.canvas['height']) + lineoffset - lineo - 1
         xe = int(self.canvas['width']) + lineoffset
         ye = int(self.canvas['height']) + lineoffset
 
         # float(self.canvas['height'])+debug_lt*4))
 
-        # NSEW unsorted list() order matters for move_line
-        self.line_matrix = {'h1': [0, y0, xe, y0],
-                            'h2': [0, y1, xe, y1],
-                            'v1': [x0, 0, x0, ye],
-                            'v2': [x1, 0, x1, ye]}
+        # line point/position
+        self.lp = {'h1': {'coord': y0},
+                   'h2': {'coord': y1},
+                   'v1': {'coord': x0},
+                   'v2': {'coord': x1}}
+        
+        # NSEW unsorted list() order matters for self.move_line
+        self.line_matrix = {}
+        for ln in ['h1', 'h2', 'v1', 'v2']:
+            lm = self.line_matrix[ln] = {}
+            p = DoubleVar(root)
+            lm['pos'] = p
+            wh = 0
+            match ln[0]:
+                case 'h':
+                    wh = int(self.canvas['height'])
+                    lm['coords'] = [0, p, xe, p]
+                case 'v':
+                    wh = int(self.canvas['width'])
+                    lm['coords'] = [p, 0, p, ye]
+            match ln[1]:
+                case '1':
+                    val = lm['conv'] = lineo
+                    p.set(val)
+                case '2':
+                    lm['conv'] = lineoffset - lineo - 1
+                    val = wh + lm['conv']
+                    p.set(val)
+            lm['ogpos'] = p.get()
+                
+        self.line_matrix_old = {'h1': [0, y0, xe, y0], 
+                                'h2': [0, y1, xe, y1], 
+                                'v1': [x0, 0, x0, ye], 
+                                'v2': [x1, 0, x1, ye]}
 
+        # for ln in self.line_matrix:
+        #     coords = self.line_matrix[ln]['coords']
+        #     for i, xy in enumerate(coords):
+        #         if type(xy) is DoubleVar:
+        #             coords[i] = xy.get()
+        #     print(self.line_matrix[ln]['coords'], self.line_matrix_old[ln], sep='\n')
+            
         # self.line_xy = {}
         # for line in list(self.line_matrix):
         #     self.line_xy[line] = self.line_matrix[line][:2]
+
+        def make_lines(**kwargs):
+            for ln in list(self.line_matrix):
+                lm = self.line_matrix[ln]
+
+                lm['line'] = self.canvas.create_line(self.get_coords(ln), **kwargs)
+                self.canvas.coords(lm['line'], self.get_coords(ln))
+                match ln[0]:
+                    case 'h':
+                        lm['direction'] = 'y'
+                    case 'v':
+                        lm['direction'] = 'x'
+
+        make_lines(width=debug_lt, fill='red')
         
-        def make_line_set(**kwargs):
-            set = {}
-            for line in list(self.line_matrix):
-                ln = self.canvas.create_line(self.line_matrix[line], **kwargs)
-                set[line] = {'canv': ln}
-            return set
+        # def make_line_set(**kwargs):
+        #     set = {}
+        #     for ln in list(self.line_matrix):
+        #         line = self.canvas.create_line(self.line_matrix[ln]['coords'], **kwargs)
+        #         set[ln] = {'canv': line}
+        #         match ln[0]:
+        #             case 'h':
+        #                 set[ln]['direction'] = 'y'
+        #             case 'v':
+        #                 set[ln]['direction'] = 'x'
+        #     return set
 
         # _init to check if closest to og pos ie. canvas edge
         # TODO make this optional; og pos != canvas edge circumstance?
         # could show in debug mode # line_set_init = make_line_set(state='hidden')
-        self.line_set = make_line_set(width=debug_lt, fill='red')
+        # self.line_set = make_line_set(width=debug_lt, fill='red')
         
-        line_set_length = len(self.line_set)
+        # line_set_length = len(self.line_set)
+        line_set_length = len(self.line_matrix)
 
         # global/instance so reusable for <B1-Motion> callback
-        self.line_index = 'h1'
-
-        def get_line(event):
-            diffs = {}
-
-            # this method only checks the necessary xy, test perfance diff?
-            for line in ['h1', 'h2']:
-                liney = abs(event.y - self.canvas.coords(self.line_set[line]['canv'])[1])
-                inity = abs(event.y - self.line_matrix[line][1])
-                diff = min(liney, inity)
-                diffs[line] = diff
-            for line in ['v1', 'v2']:
-                linex = abs(event.x - self.canvas.coords(self.line_set[line]['canv'])[0])
-                initx = abs(event.x - self.line_matrix[line][0])
-                diff = min(linex, initx)
-                diffs[line] = diff
-                            
-            # for line in self.line_set:
-            #     x = abs(event.x - self.canvas.coords(self.line_set[line]['canv'])[0])
-            #     y = abs(event.y - self.canvas.coords(self.line_set[line]['canv'])[1])
-            #     diff = min(x, y)
-            #     diffs[line] = diff
-            # for line in list(self.line_matrix):
-            #     x = abs(event.x - self.line_matrix[line][0])
-            #     y = abs(event.y - self.line_matrix[line][1])
-            #     diff = min(x, y)
-            #     diffs[line] = diff
-                
-            self.line_index = min(diffs, key=diffs.get) # TODO type hint error
-            
-            # if self.line_index >= line_set_length: # >= as 0-indexed
-            #     self.line_index -= line_set_length
-            move_line_mevent(event)
+        self.line_index = 'h1' # req. init value if spinbox used first, fix todo
+        self.line_direction = 'x' # str, again req. ^^
 
         self.min_gap = 10
 
-        def move_line_mevent(event): # mouse event
-            x = event.x
-            y = event.y
-            move_line(x, y)
-            
-        def move_line(x, y): # TODO consolidate to one param 'i'
-            # if isinstance(input, Event):
-            #     x = event.x
-            #     y = event.y
-            # else:
-            #     x = event[0]
-            #     y = event[1]
-                
-            ln = self.line_index
-            line = self.line_set[ln]['canv']
-            c = self.canvas.coords(line)
-            # ci = self.canvas.coords(line_set_init[ln])
-            # orient = ln % 2 == 0
-            # if orient and y >= ci[1]:
-            #     self.canvas.coords(line, c[0], y, c[2], y)
-            # elif not orient and x >= ci[0]:
-            #     self.canvas.coords(line, x, c[1], x, c[3])
-            # print(x, self.canvas.coords(line_set[3])[0])
-
-            # print(y, self.canvas.coords(self.line_set[ln]['canv']))
-
-            match ln:
-                case 'h1' if (y >= self.line_matrix[ln][1] and
-                              y < self.canvas.coords(self.line_set['h2']['canv'])[1]
-                              - self.min_gap):
-                    self.canvas.coords(line, c[0], y, c[2], y)
-                    self.line_set[ln]['txtvar'].set(y)
-                case 'h2' if (y <= self.line_matrix[ln][1] and
-                              y > self.canvas.coords(self.line_set['h1']['canv'])[1]
-                              + self.min_gap):
-                    self.canvas.coords(line, c[0], y, c[2], y)
-                    self.line_set[ln]['txtvar'].set(y)
-                case 'v1' if (x >= self.line_matrix[ln][0] and
-                              x < self.canvas.coords(self.line_set['v2']['canv'])[0]
-                              - self.min_gap):
-                    self.canvas.coords(line, x, c[1], x, c[3])
-                    self.line_set[ln]['txtvar'].set(x)
-                case 'v2' if (x <= self.line_matrix[ln][0] and
-                              x > self.canvas.coords(self.line_set['v1']['canv'])[0]
-                              + self.min_gap):
-                    self.canvas.coords(line, x, c[1], x, c[3])
-                    self.line_set[ln]['txtvar'].set(x)
-
         input_frame=ttk.Frame(ui_frame, width=30, height=10)
-        print(self.line_set)
+        # print(self.line_set)
 
-        for line in list(self.line_set):
-            self.line_set[line]['txtvar'] = DoubleVar(root)
+        # for line in list(self.line_set):
+        #     self.line_set[line]['txtvar'] = DoubleVar(root)
 
-        print(self.line_set)
-        def hvset(line):
-            match line: # 0=x0, 1=y0
-                case 'h1' | 'h2':
-                    xy = 1
-                case 'v1' |'v2':
-                    xy = 0
-                case _:
-                    raise Exception("Line not in line_set")                
-            self.line_set[line]['txtvar'].set(
-                self.canvas.coords(self.line_set[line]['canv'])[xy])
+        # print(self.line_set)
+        # initialises line_set txtvars
+        # def hvset(line):
+        #     match self.line_set[line]['direction']: # 0=x0, 1=y0
+        #         case 'y':
+        #             xy = 1
+        #         case 'x':
+        #             xy = 0
+        #         case _:
+        #             raise Exception("Line not in line_set")                
+        #     self.line_set[line]['txtvar'].set(
+        #         self.canvas.coords(self.line_matrix[line]['coords'])[xy])
 
-        for line in list(self.line_set):
-            hvset(line)
+        # for line in list(self.line_set):
+        #     hvset(line)
 
-        h1var = self.line_set['h1']['txtvar']
-        h1x_entry=ttk.Spinbox(input_frame, from_=0, to=100, increment=1,
-                              textvariable=h1var,
-                              command=lambda y=h1var: move_line(0, y.get())) # wrong type req int.
-        h1x_entry.grid(row=0)
-        x1 = StringVar(root, '0')
-        x1_entry=ttk.Spinbox(input_frame, textvariable=x1)
-        x1_entry.grid(row=1)
+        def make_spinbox(ln):
+            lm = self.line_matrix[ln]
+            txtvar = lm['pos']
+            return ttk.Spinbox(input_frame, increment=1, from_=0, to=100,
+                               textvariable=txtvar,
+                               command=lambda: self.move_line_spinbox(txtvar.get(), ln))
+
+        for ln in list(self.line_matrix):
+            self.line_matrix[ln]['spinbox'] = make_spinbox(ln)
+
+        for i, ln in enumerate(list(self.line_matrix)):
+            self.line_matrix[ln]['spinbox'].grid(row=i)
+        
         minimap=ttk.Frame(ui_frame, style="debug-fg.TFrame")
         input_frame.grid(column=0, row=0)
         minimap.grid(column=0, row=1, sticky=full)
@@ -321,8 +303,98 @@ class TkGui:
 
         # binds
 
-        self.canvas.bind('<Button-1>', get_line)
-        self.canvas.bind('<B1-Motion>', move_line_mevent)
+        self.canvas.bind('<Button-1>', self.get_line)
+        self.canvas.bind('<B1-Motion>', self.move_line_mevent)
+
+    def get_line(self, event):
+            diffs = {}
+
+            # this method only checks the necessary xy, test perfance diff?
+            for line in ['h1', 'h2']:
+                liney = abs(event.y - self.line_matrix[line]['pos'].get())
+                inity = abs(event.y - self.line_matrix[line]['ogpos'])
+                diff = min(liney, inity)
+                diffs[line] = diff
+            for line in ['v1', 'v2']:
+                linex = abs(event.x - self.line_matrix[line]['pos'].get())
+                initx = abs(event.x - self.line_matrix[line]['ogpos'])
+                diff = min(linex, initx)
+                diffs[line] = diff
+                            
+            # for line in self.line_set:
+            #     x = abs(event.x - self.canvas.coords(self.line_set[line]['canv'])[0])
+            #     y = abs(event.y - self.canvas.coords(self.line_set[line]['canv'])[1])
+            #     diff = min(x, y)
+            #     diffs[line] = diff
+            # for line in list(self.line_matrix):
+            #     x = abs(event.x - self.line_matrix[line][0])
+            #     y = abs(event.y - self.line_matrix[line][1])
+            #     diff = min(x, y)
+            #     diffs[line] = diff
+                
+            self.line_index = min(diffs, key=diffs.get) # TODO type hint error
+            self.line_direction = self.line_matrix[self.line_index]['direction']
+            
+            # if self.line_index >= line_set_length: # >= as 0-indexed
+            #     self.line_index -= line_set_length
+            self.move_line_mevent(event)
+
+    def get_coords(self, ln):
+        lm = self.line_matrix[ln]['coords']
+        match ln[0]:
+            case 'h': return tuple(x.get() if type(x) is DoubleVar else x for x in lm)
+            case 'v': return tuple(x.get() if type(x) is DoubleVar else x for x in lm)
+        
+            
+    def move_line_mevent(self, event): # mouse event
+        self.move_line(getattr(event, self.line_direction)) # getattr speed?
+        
+    def move_line_spinbox(self, i, line):
+        self.line_index = line
+        self.move_line(i)
+        
+    def move_line(self, i):
+        # if isinstance(input, Event):
+        #     x = event.x
+        #     y = event.y
+        # else:
+        #     x = event[0]
+        #     y = event[1]
+
+        ln = self.line_index
+        lm = self.line_matrix
+
+        # ci = self.canvas.coords(line_set_init[ln])
+        # orient = ln % 2 == 0
+        # if orient and y >= ci[1]:
+        #     self.canvas.coords(line, c[0], y, c[2], y)
+        # elif not orient and x >= ci[0]:
+        #     self.canvas.coords(line, x, c[1], x, c[3])
+        # print(x, self.canvas.coords(line_set[3])[0])
+
+        # print(y, self.canvas.coords(self.line_set[ln]['canv']))
+
+        def update(ln, i):
+            lm[ln]['pos'].set(i)
+            coords = self.get_coords(ln)
+            self.canvas.coords(lm[ln]['line'], coords)
+
+        # inequality differs between 1/2
+        # coordinate (matrix, parallel & self) differs between hor/vert
+        # parallel id differs, but naturally paired between h or v
+        match ln:
+            case 'h1' if (i >= lm[ln]['ogpos'] and
+                          i < lm['h2']['pos'].get() - self.min_gap):
+                update(ln, i)
+            case 'h2' if (i <= lm[ln]['ogpos'] and
+                          i > lm['h1']['pos'].get() + self.min_gap):
+                update(ln, i)
+            case 'v1' if (i >= lm[ln]['ogpos'] and
+                          i < lm['v2']['pos'].get() - self.min_gap):
+                update(ln, i)
+            case 'v2' if (i <= lm[ln]['ogpos'] and
+                          i > lm['v1']['pos'].get() + self.min_gap):
+                update(ln, i)
 
     # scroll guillotine with bar
     def image_scale(self, root, img, factor):
@@ -332,13 +404,12 @@ class TkGui:
         else:
             scaled = img.zoom(factor)
         self.canvas.itemconfigure(self.pdfview, image=scaled)
-        self.canvas['scrollregion']=(0,0,scaled.width(),
-                                        scaled.height())
+        self.canvas['scrollregion']=(0, 0, scaled.width(), scaled.height())
         root.image = scaled # maintain reference w/o PIL
         return scaled
 
     def canvas_fit(self, img):
-        self.canvas['width'] = img.width()   + (self.lt - self.bd) * 2
+        self.canvas['width']  = img.width()  + (self.lt - self.bd) * 2
         self.canvas['height'] = img.height() + (self.lt - self.bd) * 2
 
     # def addLabel(self, txt):
@@ -352,3 +423,24 @@ if __name__ == "__main__":
     TkGui(root)
     root.mainloop()
  
+# if __name__ == "__main__":
+#     import asyncio
+#     import logging
+    
+#     # LOGGING.basicConfig(level=logging.DEBUG)
+#     # logging.getLogger("asyncio").setLevel(logging.WARNING)
+#     windll.shcore.SetProcessDpiAwareness(2)
+#     root = Tk()
+#     gui = TkGui(root)
+
+#     async def async_input():
+#         loop = asyncio.get_running_loop()
+#         await loop.run_in_executor(
+#             None, input)
+#     async def updater():
+#         while True:
+#             root.update()
+#             await asyncio.sleep(0.2)
+#             await asyncio.async_input()
+
+#     asyncio.run(updater(), debug=True)
